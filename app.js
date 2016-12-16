e23d6453b168cf3a9c6be8370beb060f92f2a02c
Daniel Alonte <djalonte@gmail.com>
2016-12-15 20:55:54 -0500

    added multer

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
-
 var bodyParser = require('body-parser');
+var multer = require ('multer')
+var upload = multer({ dest: 'entries/' })
 
 app.use(bodyParser.urlencoded({ extended: false}));
 
@@ -18,8 +19,9 @@ app.get('/entries', function(req, res) {
 	res.render('entries');
 });
 
-app.post('/entry', function(req, res) {
+app.post('/entries', upload.single('photo'), function(req, res) {
 	console.log(req.body);
+	console.log(req.file);
 	res.render('entries', {data: req.body});
 });
 
